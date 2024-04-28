86380303d0e87b5be3f8476cb22338f4fa41bf91
vandunxg <nguyenvandung.official@gmail.com>
2024-04-28 16:25:02 +0700

    init: init project

---
+++ src/test/java/com/vandunxg/fooddelivery/FoodDeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandunxg.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
